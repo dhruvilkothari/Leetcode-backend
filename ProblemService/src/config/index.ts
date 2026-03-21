da5816c0566323b4ef77a6b6bed1c5c7f72ea321
Dhruvil Kothari <kotharidhruvil3@gmail.com>
2026-03-21 12:58:10 +0530

    Added Mongo setup and Problem Schema

---
+++ ProblemService/src/config/index.ts
@@ -0,0 +1,19 @@
+import dotenv from 'dotenv';
+
+type ServerConfig = {
+    PORT: number,
+    DB_URL: string
+}
+
+function loadEnv() {
+    dotenv.config();
+    console.log(`Environment variables loaded`);
+}
+
+loadEnv();
+
+export const serverConfig: ServerConfig = {
+    PORT: Number(process.env.PORT) || 3001,
+    DB_URL: process.env.DB_URL as string
+
+};
---
+++ ProblemService/src/models/problem.model.ts
@@ -0,0 +1,44 @@
+import mongoose from "mongoose";
+import { Document } from "mongoose";
+
+
+export interface ItestCase extends Document {
+    input: string;
+    output: string;
+}
+
+export interface IProblem extends Document {
+
+    title: string;
+    description: string;
+    difficulty: "Easy" | "Medium" | "Hard";
+    editorial?: string;
+    testCases: ItestCase[];
+    createdAt: Date;
+    updatedAt: Date;
+}
+
+const arrayLimit = (val: ItestCase[]) => val.length > 0;
+
+const testCaseSchema = new mongoose.Schema<ItestCase>({
+    input: { type: String, required: [true, "Input is Required"] },
+    output: { type: String, required: [true, "Output is Required"] }
+}, 
+// { _id: false }   
+);
+
+const problemSchema = new mongoose.Schema<IProblem>({
+    title: { type: String, required: [true, "Title is Required"], maxlength: [100, "Title cannot exceed 100 characters"], trim: true },
+    description: { type: String, required: [true, "Description is Required"], trim: true },
+    difficulty: { type: String, enum: {values: ["Easy", "Medium", "Hard"], message: "{VALUE} is not a valid difficulty level"}, required: [true, "Difficulty is Required"] },
+    editorial: { type: String, trim: true },
+    testCases: [testCaseSchema, { validate: [arrayLimit, "At least one test case is required"] }]
+}, { timestamps: true });
+
+problemSchema.index({ title: 1 }, { unique: true });
+problemSchema.index({ difficulty: 1 });
+
+const Problem = mongoose.model<IProblem>("Problem", problemSchema);
+
+export default Problem;
+
